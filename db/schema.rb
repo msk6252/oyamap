d3a8f1983a7abb0aaa0831ade943b319a0b46511
msk6252 <msk6252@yahoo.co.jp>
2016-12-30 14:00:25 +0900

    [WIP][add] geocoderの追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161228094946) do
+ActiveRecord::Schema.define(version: 20161229173642) do
 
   create_table "maps", force: :cascade do |t|
     t.string   "title"
