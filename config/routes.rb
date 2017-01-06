b89d14fd1e00c4519de2fe1f7920b42e8dd8058d
msk6252 <msk6252@yahoo.co.jp>
2017-01-06 18:36:14 +0900

    [add] admin準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :admin_users
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   get 'home/index'
   root 'maps#index'
   resources :maps
