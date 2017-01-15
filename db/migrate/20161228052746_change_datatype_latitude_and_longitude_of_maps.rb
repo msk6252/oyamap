8beac07a33a37c7d5ff795bf665d59959330b3c5
msk6252 <msk6252@yahoo.co.jp>
2017-01-15 13:45:52 +0900

    [modify] マイグレーションファイル削除

--- db/migrate/20161228052746_change_datatype_latitude_and_longitude_of_maps.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDatatypeLatitudeAndLongitudeOfMaps < ActiveRecord::Migration
-  def change
-  	change_column :maps, :latitude, :double
-  	change_column :maps, :longitude, :double
-  	
-  end
-end
